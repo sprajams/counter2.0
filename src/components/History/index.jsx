0c0b0247d6f30ed0b3a2ecc27741deb8f6383f7f
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-14 15:57:42 -0700

    styling of history component

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -4,19 +4,18 @@ function History({ data }) {
   return (
     <div>
       <h2 className={styles.title}>History</h2>
-      <div className={styles.container}>
+      <div className={data.length > 1 ? styles.container : null}>
         {data.map((x, i) => {
-          if (i > 0) {
-            return (
-              <div key={i} className={styles.wrap}>
-                <div>#{i}</div>
-                <div>{x.action}</div>
-                <div className={styles.transformation}>
-                  {x.before} ➜ {x.after}
-                </div>
+          return i > 0 ? (
+            <div key={i} className={styles.wrap}>
+              <div className={styles.num}>#{i}</div>
+              <div className={styles.action}>{x.action}</div>
+
+              <div>
+                ({x.before} ➜ {x.after})
               </div>
-            );
-          }
+            </div>
+          ) : null;
         })}
       </div>
     </div>
