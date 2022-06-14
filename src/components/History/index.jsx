1c54586a3a6af7d0e7d56a5b6f3ce2e105847c83
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-14 13:41:00 -0700

    handle undo and redo but only once

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -3,15 +3,18 @@ import styles from "./styles.module.scss";
 function History({ data }) {
   return (
     <div>
-      <h2>History</h2>
+      <h2 className={styles.title}>History</h2>
       {data.map((x, i) => {
-        return (
-          <div key={i} className={styles.wrap}>
-            <div>{x.action}</div>
-            <div>{x.before}</div>
-            <div>{x.after}</div>
-          </div>
-        );
+        if (i > 0) {
+          return (
+            <div key={i} className={styles.wrap}>
+              <div>#{i}</div>
+              <div>{x.action}</div>
+              <div>{x.before}</div>
+              <div>{x.after}</div>
+            </div>
+          );
+        }
       })}
     </div>
   );
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import History from "../History";
 import styles from "./styles.module.scss";
 function Main() {
-  const [sum, setSum] = useState(0);
+  const [total, setTotal] = useState(0);
   const [history, setHistory] = useState([
     {
       action: "",
@@ -10,22 +10,65 @@ function Main() {
       after: "",
     },
   ]);
+
+  const [undo, setUndo] = useState();
+  const [redo, setRedo] = useState();
+
   const minusOne = () => {
-    setSum(sum - 1);
-    setHistory([...history, { action: -1, before: sum, after: sum - 1 }]);
+    setTotal(total + -1);
+    setHistory([...history, { action: -1, before: total, after: total + -1 }]);
   };
   const plusOne = () => {
-    setSum(sum + 1);
+    setTotal(total + 1);
   };
+
+  const undoAction = () => {
+    //remove last object in history arr and store it into a state
+    if (history.length > 1) {
+      setUndo(history.pop());
+    }
+  };
+
+  const redoAction = () => {
+    history.push(redo);
+    setTotal(redo.after);
+    setRedo();
+  };
+  useEffect(() => {
+    // if undo exists, then reset total to previous number and set undo back to undefined
+    if (undo) {
+      setTotal(undo.before);
+      setRedo(undo);
+      setUndo();
+      console.log(undo, "u");
+      console.log(redo, "r");
+    }
+  }, [undo, total, redo]);
+
   return (
     <div className={styles.outer}>
+      <div>
+        <button onClick={undoAction}>UNDO</button>
+        <button
+          onClick={redoAction}
+          disabled={redo === undefined ? true : false}
+        >
+          REDO
+        </button>
+      </div>
       <div className={styles.main}>
         <button onClick={minusOne}>-1</button>
-        <div>{sum}</div>
+        <div>{total}</div>
         <button onClick={plusOne}>+1</button>
       </div>
-
       <History data={history} />
+      {undo ? (
+        <div>
+          {undo.action}
+          {undo.before}
+          {undo.after}
+        </div>
+      ) : null}
     </div>
   );
 }
